27d1b6b68bdcc610872a83cf90d1ad5a6a214f1e
Bhuvan Krishna <bhuvan@swecha.net>
2023-03-07 17:06:38 +0530

    Added basic styling

--- src/pages/paintings/index.tsx
+++ src/pages/paintings/index.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import { fetchData, ResponseData } from '../api/fetch';
+import styles from './Paintings.module.css';
+
+
 
 interface Props {}
-
 function MyComponent(props: Props) {
   const [data, setData] = useState<ResponseData | null>(null);
   const [error, setError] = useState<Error | null>(null);
@@ -30,19 +32,44 @@ function MyComponent(props: Props) {
   }
 
   return (
-    <div>
+
+    <div className={styles.paintingsMainContent} id="paintings-main">
       {data.data.map((item) => (
-        <div key={item.id}>
-          <h1>{item.attributes.Title}</h1>
+        <div key={item.id} className={styles.paintingsCard}>
+          <div className={styles.paintingsFeaturedImage}>
+            <a>
+              <img
+                src={BASE_URL+item.attributes.Painting.data.attributes.formats.small.url}
+                className="attachment-medium size-medium wp-post-image"
+                alt=""
+                width="300"
+                height="234"
+                decoding="async"
+                loading="lazy"
+              />
+            </a>
+
+          </div>
+          <a>
+            <img 
+              className={styles.paintingsPainterImg} 
+              src={BASE_URL+item.attributes.ChildPhoto.data.attributes.formats.thumbnail.url}
+            />
+          </a>
+          <div className={styles.paintingsPainterName}>
+            <h2 className={styles.entryTitle}>
+              <a rel="bookmark">{item.attributes.Name}</a>
+            </h2>
+          </div>
+          {/* <h1>{item.attributes.Title}</h1>
           <p>{item.attributes.Name}</p>
           <p>{item.attributes.createdAt}</p>
           <p>{item.attributes.Painting.data.attributes.name}</p>
           <p>{item.attributes.Painting.data.attributes.formats.small.name}</p>
           <p>{item.attributes.Painting.data.attributes.formats.thumbnail.name}</p>
-          <img src={BASE_URL+item.attributes.Painting.data.attributes.formats.small.url}/>
-          <img src={BASE_URL+item.attributes.ChildPhoto.data.attributes.formats.thumbnail.url}/>
-          <a href={item.attributes.VideoLink}>Video</a>
+          <a href={item.attributes.VideoLink}>Video</a> */}
         </div>
+
       ))}
     </div>
   );
